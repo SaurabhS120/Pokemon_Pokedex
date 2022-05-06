3fc3e07fb720c03c93347024e6607a985f25eebe
Saurabh K. Sonar <saurabh.sonar120@gmail.com>
2022-05-06 16:36:59 +0530

    pokemon list activity layout created

--- app/src/main/java/com/example/pokemon/PokemonListActivity.kt
+++ app/src/main/java/com/example/pokemon/PokemonListActivity.kt
@@ -3,9 +3,11 @@ package com.example.pokemon
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class PokemonListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_pokemon_list)
+        supportActionBar?.hide()
+
     }
 }
